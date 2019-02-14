bb589c3805451e56a43a4828a68d4fd62b332416
Syed Adiba Shuhin <adibashuhin@gmail.com>
2019-02-14 12:36:38 +0600

    Add files via upload

---
+++ GrepLike.js
@@ -0,0 +1,14 @@
+let fs = require ('fs');
+let readLine = require ('readline');
+
+let ProcessQuery  = process.argv.slice(2);
+
+let readlineInterFace = readLine.createInterface({
+  input: fs.createReadStream(`./${ ProcessQuery[1]}`)
+});
+
+readlineInterFace.on("line", line => {
+  if(line.includes(ProcessQuery[0])){
+    console.log(line);
+  }
+});
